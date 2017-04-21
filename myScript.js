e80f5e9240bad98d8c9028b7e5734149d8fd0931
halulu1003 <youyujia1003@outlook.com>
2017-04-21 20:23:17 +1200

    js

--- myScript.js
+++ myScript.js
@@ -1,12 +1,4 @@
-var hotel1 = {
-    name : "Whetu Apartments Hotel",
-    address  : "Doe",
-    phone       : 50,
-};
-
-document.getElementById("hotel1").innerHTML =
-hotel1.name + " is located in " + hotel1.address;
-
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -23,22 +15,60 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-var slideIndex = 0;
-showSlides();
+window.onload = function(){
+ var star = document.getElementById("star");
+ var star_li = star.getElementsByTagName("li");
+ var result = document.getElementById("result");
+ var i=0;
+ var len = star_li.length;
+ for(i=0; i<len; i++){
+ star_li[i].index = i;
+ star_li[i].onclick = function(){
+    for(i=0; i<len; i++){
+   star_li[i].className = "";
+  }
+ for(i=0; i<=this.index; i++){
+   star_li[i].className = "act";
+  }
+    result.innerHTML = (this.index+1);
+ } 
+ }
+}
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+//slide show
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
